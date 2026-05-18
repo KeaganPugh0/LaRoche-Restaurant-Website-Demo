c548ed36c62128018c95bb578b4ab1a57f9c8b50
KeaganPugh <ST10467075@vcconnect.edu.za>
2026-05-18 21:22:37 +0200

    Updated menu page design and added PDF menu links

--- script.js
+++ script.js
@@ -84,3 +84,10 @@ musicToggle.addEventListener("click", () => {
     }
 
 });
+
+const hamburgerBtn = document.getElementById("hamburgerBtn");
+const mobileMenu = document.getElementById("mobileMenu");
+
+hamburgerBtn.addEventListener("click", () => {
+    mobileMenu.classList.toggle("active");
+});
